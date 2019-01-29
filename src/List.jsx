ece44ce4525f7a7e87988f3f770d548845ebfc7c
Josiahngugi <mainajosiah95@gmail.com>
2019-01-29 08:42:38 -0500

    displaying traits in a table

--- src/List.jsx
+++ src/List.jsx
@@ -10,10 +10,34 @@
         traits.map((property) =>{
           console.log(property);
           return(
-            <div key={property.url}>
-              <h1 className ="char-name">{property.name}</h1>
-              <Info Info ={property} />
-            </div>
+            <div className="container">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Star Name</th>
+                  <th>Characters</th>
+                </tr>
+              </thead>
+              <tbody>
+                {traits.map(obj =>{
+                  return(
+                    <tr>
+                      <td>
+                      {obj.name}
+                      </td>
+                      <td>
+                       <Info Info ={property} />
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+              </table>
+              </div>
+
+            // <div key={property.url}>
+            //   <h5 className ="char-name">{property.name}  <span>&nbsp;&nbsp;</span> <Info Info ={property} /></h5>
+            // </div>
           )
         })
       }
