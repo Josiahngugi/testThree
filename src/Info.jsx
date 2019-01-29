849269e82240ccd517cddef5db1d21df911d3916
Josiahngugi <mainajosiah95@gmail.com>
2019-01-29 06:11:36 -0500

    adding show info button

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,6 +1,24 @@
  import React, { Component } from 'react';
 class Info extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      expanded: false,
+    }
+  }
+
+   open(){
+     this.setState({expanded: !this.state.expanded})
+   }
+   close(){
+     this.setState({expanded: !this.state.expanded})
+   }
   render(){
+    this.Info =this.props.Info;
+     if(!this.state.expanded){
+       return <p className="btn btn-info" onClick={this.open}>show info</p>
+     }
     return(
       <div></div>
     )
